d6afe8db4e4023b997eeaf9e1f2c793696cf197e
zFry <99491170+zFrxncesck1@users.noreply.github.com>
2026-03-24 23:42:10 +0100

    Fix regex for maxSize replacement in plugin

--- src/userplugins/dontLimitMe/index.tsx
+++ src/userplugins/dontLimitMe/index.tsx
@@ -11,12 +11,12 @@ export default definePlugin({
     description: "removes the limit on message sending. spam away!",
     authors: [{ name: "Death", id: 1003477997728313405n }],
     patches: [
-        {
-            find: "cancelQueueMetricTimers",
-            replacement: {
-                match: /this\.maxSize=[a-zA-Z]+,/,
-                replace: "this.maxSize=Number.MAX_SAFE_INTEGER,"
-            }
-        }
+        {
+            find: "cancelQueueMetricTimers",
+            replacement: {
+                match: /this\.maxSize=\w+/,
+                replace: "this.maxSize=Number.MAX_SAFE_INTEGER"
+            }
+        }
     ]
 });
