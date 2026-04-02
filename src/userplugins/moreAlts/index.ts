6ec09fc38e5f90ca5d8fdfe09b4501d8d1c7a650
zFry <99491170+zFrxncesck1@users.noreply.github.com>
2026-04-02 16:33:23 +0200

    Add files via upload

---
+++ src/userplugins/moreAlts/index.ts
@@ -0,0 +1,31 @@
+import { definePluginSettings } from "@api/Settings";
+import definePlugin, { OptionType } from "@utils/types";
+const settings = definePluginSettings({
+    amtOfAcounts: {
+        default: 10,
+        type: OptionType.NUMBER,
+        description: "The amount of alts to allow."
+    },
+});
+
+export default definePlugin({
+    name: "MoreAlts",
+    description: "Allows you to have more alts in the account switcher",
+    authors: [
+        {
+            id: 253302259696271360n,
+            name: "zastix",
+        },
+    ],
+    settings,
+    patches: [
+        {
+            find: "\"multiaccount_cta_tooltip_seen\"",
+            replacement: [{
+                // the first export seems to always be the amount of alts, we should find a better way to do this in the future
+                match: /(.{0,2}):function\(\){return .{1,2}\}/,
+                replace: `$1:function(){return $self.settings.amtOfAcounts}`
+            }]
+        }
+    ]
+});
