bbbd72f0e48209d83b6c436f56ce9b3ea282b561
zFry <99491170+zFrxncesck1@users.noreply.github.com>
2026-04-20 21:45:05 +0200

    Add files via upload

--- src/userplugins/RamCleaner/index.tsx
+++ src/userplugins/RamCleaner/index.tsx
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import managedStyle from "./styles.css?managed";
+import managedStyle from "./style.css?managed";
 
 import { definePluginSettings } from "@api/Settings";
 import { Logger } from "@utils/Logger";
