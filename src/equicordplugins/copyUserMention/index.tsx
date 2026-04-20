2c0ecdd164359fd24952e6ec80a33aa00cc27aac
1xstore <ryankharj@gmail.com>
2026-04-20 11:57:56 +0300

    Update index.tsx

--- src/equicordplugins/copyUserMention/index.tsx
+++ src/equicordplugins/copyUserMention/index.tsx
@@ -46,7 +46,7 @@ const UserContextMenuPatch: NavContextMenuPatchCallback = (children, { user }: U
 
 export default definePlugin({
     name: "CopyUserMention",
-    authors: [EquicordDevs.Cortex, Devs.castdrian],
+    authors: [EquicordDevs.Cortex, Devs.castdrianو, Devs.rz30],
     description: "Adds a button to copy user's mention on the user context menu, works best with ValidUser.",
     tags: ["Chat", "Utility"],
     contextMenus: {
