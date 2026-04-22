d0cf5c0a1bf8fd1e30f24d7547d598404cffbbe4
zFry <99491170+zFrxncesck1@users.noreply.github.com>
2026-04-22 02:53:28 +0200

    Update Vencord.ts

--- src/Vencord.ts
+++ src/Vencord.ts
@@ -151,7 +151,7 @@ async function runUpdateCheck() {
                 notifiedForUpdatesThisSession = true;
 
                 showNotice(
-                    "Equicord has been updated!",
+                    "TrashCord has been updated!",
                     "Restart",
                     relaunch
                 );
@@ -163,7 +163,7 @@ async function runUpdateCheck() {
         notifiedForUpdatesThisSession = true;
 
         showNotice(
-            "A new version of Equicord is available!",
+            "A new version of TrashCord is available!",
             "View Update",
             () => openSettingsTabModal(UpdaterTab!)
         );
@@ -181,7 +181,7 @@ function initTrayIpc() {
             VencordNative.tray.setUpdateState(isOutdated);
 
             if (isOutdated) {
-                showNotice("An Equicord update is available!", "View Update", () => openSettingsTabModal(UpdaterTab!));
+                showNotice("An TrashCord update is available!", "View Update", () => openSettingsTabModal(UpdaterTab!));
             } else {
                 showNotice("No updates available, you're on the latest version!", "OK", popNotice);
             }
@@ -196,7 +196,7 @@ function initTrayIpc() {
             await update();
             relaunch();
         } catch (err) {
-            UpdateLogger.error("Failed to repair Equicord", err);
+            UpdateLogger.error("Failed to repair TrashCord", err);
         }
     });
 
@@ -226,7 +226,7 @@ async function init() {
                 "Webpack has finished initialising, but some patches haven't been applied yet.",
                 "This might be expected since some Modules are lazy loaded, but please verify",
                 "that all plugins are working as intended.",
-                "You are seeing this warning because this is a Development build of Equicord.",
+                "You are seeing this warning because this is a Development build of TrashCord.",
                 "\nThe following patches have not been applied:",
                 "\n\n" + pendingPatches.map(p => `${p.plugin}: ${p.find}`).join("\n")
             );
