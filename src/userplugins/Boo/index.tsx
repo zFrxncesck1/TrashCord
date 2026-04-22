213307d5984f4933bfab2c77e751334f77cd1aa8
zFry <99491170+zFrxncesck1@users.noreply.github.com>
2026-04-22 03:13:36 +0200

    Add files via upload

---
+++ src/userplugins/Boo/index.tsx
@@ -0,0 +1,49 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+/*
+ * Fixxed by zFrxncesck1 
+ */
+
+import managedStyle from "./components/styles.css?managed";
+import ErrorBoundary from "@components/ErrorBoundary";
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+
+import Boo from "./components/Boo";
+import { BooProps } from "./types";
+
+export default definePlugin({
+    name: "Boo",
+    description: "A cute ghost will appear if you don't answer their DMs",
+    authors: [Devs.x2b],
+    tags: ["Fun", "Chat"],
+    enabledByDefault: false,
+    managedStyle,
+
+     start() {
+     },
+     stop() {
+     },
+
+    patches: [
+        {
+            find: "interactiveSelected]",
+            replacement: {
+                match: /interactiveSelected.{0,50}children:\[/,
+                replace: "$&$self.renderBoo(arguments[0]),"
+            }
+        }
+    ],
+
+    renderBoo: (props: BooProps) => {
+        return (
+            <ErrorBoundary noop>
+                <Boo {...props} />
+            </ErrorBoundary>
+        );
+    }
+});
